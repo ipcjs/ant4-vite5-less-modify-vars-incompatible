92fdc8ddcea5263f0d8cb9462014789ed2f928ee
ipcjs.mac <gipcjs@gmail.com>
2024-06-05 19:39:55 +0800

    add ant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import {Button} from 'ant-design-vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+    <Button type="primary">Ant Button</Button>
     <TheWelcome />
   </main>
 </template>
